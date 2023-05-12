aa25ec36750e702079f8cee4ec93b0982292010e
ienaga <ienaga@tvon.jp>
2023-05-12 22:47:21 +0900

    macos、windows書き出しを調整

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ if (15 > version.split(".")[0]) {
     process.exit(1);
 }
 
-const { spawn, execSync } = require("child_process");
 const fs = require("fs");
 const webpackConfig = require(`${process.cwd()}/webpack.config.js`);
 const electronBuilder = require("electron-builder");
@@ -24,29 +23,32 @@ let debug       = false;
 
 for (let idx = 0; idx < process.argv.length; ++idx) {
 
-    const value = process.argv[idx];
-    if (value.indexOf("--help") > -1 || value.indexOf("-h") > -1) {
-        hasHelp = true;
+    switch (process.argv[idx]) {
+
+        case "--debug":
+            debug = true;
+            break;
+
+        case "--help":
+        case "--h":
+            hasHelp = true;
+            break;
+
+        case "--platform":
+            platform = process.argv[++idx].toLowerCase();
+            break;
+
+        case "--env":
+            environment = process.argv[++idx];
+            break;
+
+        default:
+            break;
+
+    }
+
+    if (hasHelp) {
         break;
-    }
-
-    if (value.indexOf("--platform=") > -1) {
-        platform = value
-            .split("=")
-            .pop()
-            .replace("\"", "")
-            .toLowerCase();
-    }
-
-    if (value.indexOf("--env=") > -1) {
-        environment = value
-            .split("=")
-            .pop()
-            .replace("\"", "");
-    }
-
-    if (value.indexOf("--debug") > -1) {
-        debug = true;
     }
 }
 
@@ -58,10 +60,17 @@ if (hasHelp) {
     console.log();
     console.log("Export with `--platform` and `--env` arguments.");
     console.log();
-    console.log("For example:");
-    console.log("npx @next2d/builder --platform=web --env=prd");
+    console.log("For build example:");
+    console.log("npx @next2d/builder --platform web --env prd");
+    console.log();
+    console.log("For debug example:");
+    console.log("npx @next2d/builder --debug --platform web --env prd");
     console.log();
     process.exit(1);
+}
+
+if (!fs.existsSync(`${webpackConfig.output.path}`)) {
+    fs.mkdirSync(`${webpackConfig.output.path}`, { "recursive": true });
 }
 
 /**
@@ -102,15 +111,43 @@ const buildHTML = (file_path, product_name) =>
  * @description Web用アプリの書き出し関数
  *              Export function for web applications
  *
- * @return {void}
+ * @return {Promise}
  * @method
  * @public
  */
 const buildWeb = () =>
 {
-    execSync(
-        `${process.cwd()}/node_modules/.bin/webpack --mode production --env=${environment} --platform=${platform}`
-    );
+    return new Promise((resolve) =>
+    {
+        const cp = require("child_process");
+
+        const command = `${process.cwd()}/node_modules/.bin/webpack`;
+
+        const stream = cp.spawn(command, [
+            "--mode",
+            "production",
+            "--env",
+            environment
+        ]);
+
+        stream.on("error", (error) =>
+        {
+            console.error(error);
+            process.exit(1);
+        });
+
+        stream.on("data", (data) =>
+        {
+            console.log("koko");
+            console.log(data.toString());
+        });
+
+        stream.on("exit", () =>
+        {
+            console.log("build web app.");
+            resolve();
+        });
+    });
 };
 
 /**
@@ -204,7 +241,7 @@ const setElectronIndexPath = () =>
     fs.writeFileSync(
         `${process.cwd()}/electron.index.json`,
         JSON.stringify({
-            "path":  htmlFilePath.replace(process.cwd(), ".")
+            "path": htmlFilePath.replace(process.cwd(), ".").trim()
         }, null, 2)
     );
 };
@@ -239,71 +276,77 @@ const buildWindow = () =>
      * web版の書き出し
      * Exporting the web version
      */
-    buildWeb();
-
-    /**
-     * Electronの書き出しに必要な設定を生成
-     * Generate settings necessary for exporting Electron
-     */
-    const config = generateElectronConfig("windows");
-
-    /**
-     * 読み込みようのindex.htmlファイルがなければ生成
-     * If there is no index.html file to load, generate one.
-     */
-    const htmlFile      = `${webpackConfig.output.path}/index.html`;
-    const generatedHTML = buildHTML(htmlFile, config.productName);
-
-    /**
-     * Electronで読み込むindex.htmlのパスをセット
-     * Set the path to index.html to load in Electron
-     */
-    setElectronIndexPath();
-
-    if (debug) {
-
-        const proc = spawn(
-            `${process.cwd()}/node_modules/.bin/electron`,
-            [
-                `${process.cwd()}/electron.js`,
-                `--env=${environment}`
-            ]
-        );
-
-        proc.stdout.on("close", () =>
+    buildWeb()
+        .then(() =>
         {
-            if (generatedHTML) {
-                fs.unlinkSync(htmlFile);
+            /**
+             * Electronの書き出しに必要な設定を生成
+             * Generate settings necessary for exporting Electron
+             */
+            const config = generateElectronConfig("windows");
+
+            /**
+             * 読み込みようのindex.htmlファイルがなければ生成
+             * If there is no index.html file to load, generate one.
+             */
+            const htmlFile      = `${webpackConfig.output.path}/index.html`;
+            const generatedHTML = buildHTML(htmlFile, config.productName);
+
+            /**
+             * Electronで読み込むindex.htmlのパスをセット
+             * Set the path to index.html to load in Electron
+             */
+            setElectronIndexPath();
+
+            if (debug) {
+
+                const cp = require("child_process");
+                const stream = cp.spawn(
+                    `${process.cwd()}/node_modules/.bin/electron`,
+                    [
+                        `${process.cwd()}/electron.js`,
+                        "--env",
+                        environment
+                    ]
+                );
+
+                stream.on("close", () =>
+                {
+                    if (generatedHTML) {
+                        fs.unlinkSync(htmlFile);
+                    }
+
+                    unlinkElectronIndexPath();
+                });
+
+                stream.on("destroy", () =>
+                {
+                    if (generatedHTML) {
+                        fs.unlinkSync(htmlFile);
+                    }
+
+                    unlinkElectronIndexPath();
+                });
+
+            } else {
+
+                electronBuilder
+                    .build({
+                        "projectDir": process.cwd(),
+                        "targets": electronBuilder.Platform.WINDOWS.createTarget(),
+                        "config": config
+                    })
+                    .then(() =>
+                    {
+                        if (generatedHTML) {
+                            fs.unlinkSync(htmlFile);
+                        }
+
+                        unlinkElectronIndexPath();
+                    });
+
             }
         });
-
-        proc.stdout.on("destroy", () =>
-        {
-            if (generatedHTML) {
-                fs.unlinkSync(htmlFile);
-            }
-
-            unlinkElectronIndexPath();
-        });
-
-    } else {
-
-        electronBuilder
-            .build({
-                "projectDir": process.cwd(),
-                "targets": electronBuilder.Platform.WINDOWS.createTarget(),
-                "config": config
-            })
-            .then(() =>
-            {
-                if (generatedHTML) {
-                    fs.unlinkSync(htmlFile);
-                }
-
-                unlinkElectronIndexPath();
-            });
-
-    }
 };
 
 /**
@@ -320,71 +363,78 @@ const buildMac = () =>
      * web版の書き出し
      * Exporting the web version
      */
-    buildWeb();
-
-    /**
-     * Electronの書き出しに必要な設定を生成
-     * Generate settings necessary for exporting Electron
-     */
-    const config = generateElectronConfig("macos");
-
-    /**
-     * 読み込みようのindex.htmlファイルがなければ生成
-     * If there is no index.html file to load, generate one.
-     */
-    const htmlFile      = `${webpackConfig.output.path}/index.html`;
-    const generatedHTML = buildHTML(htmlFile, config.productName);
-
-    /**
-     * Electronで読み込むindex.htmlのパスをセット
-     * Set the path to index.html to load in Electron
-     */
-    setElectronIndexPath();
-
-    if (debug) {
-
-        const proc = spawn(
-            `${process.cwd()}/node_modules/.bin/electron`,
-            [
-                `${process.cwd()}/electron.js`,
-                `--env=${environment}`
-            ]
-        );
-
-        proc.stdout.on("close", () =>
+    buildWeb()
+        .then(() =>
         {
-            if (generatedHTML) {
-                fs.unlinkSync(htmlFile);
+            /**
+             * Electronの書き出しに必要な設定を生成
+             * Generate settings necessary for exporting Electron
+             */
+            const config = generateElectronConfig("macos");
+
+            /**
+             * 読み込みようのindex.htmlファイルがなければ生成
+             * If there is no index.html file to load, generate one.
+             */
+            const htmlFile      = `${webpackConfig.output.path}/index.html`;
+            const generatedHTML = buildHTML(htmlFile, config.productName);
+
+            /**
+             * Electronで読み込むindex.htmlのパスをセット
+             * Set the path to index.html to load in Electron
+             */
+            setElectronIndexPath();
+
+            if (debug) {
+
+                const cp = require("child_process");
+
+                const stream = cp.spawn(
+                    `${process.cwd()}/node_modules/.bin/electron`,
+                    [
+                        `${process.cwd()}/electron.js`,
+                        "--env",
+                        environment
+                    ]
+                );
+
+                stream.on("close", () =>
+                {
+                    if (generatedHTML) {
+                        fs.unlinkSync(htmlFile);
+                    }
+
+                    unlinkElectronIndexPath();
+                });
+
+                stream.on("destroy", () =>
+                {
+                    if (generatedHTML) {
+                        fs.unlinkSync(htmlFile);
+                    }
+
+                    unlinkElectronIndexPath();
+                });
+
+            } else {
+
+                electronBuilder
+                    .build({
+                        "projectDir": process.cwd(),
+                        "targets": electronBuilder.Platform.MAC.createTarget(),
+                        "config": config
+                    })
+                    .then(() =>
+                    {
+                        if (generatedHTML) {
+                            fs.unlinkSync(htmlFile);
+                        }
+
+                        unlinkElectronIndexPath();
+                    });
+
             }
         });
-
-        proc.stdout.on("destroy", () =>
-        {
-            if (generatedHTML) {
-                fs.unlinkSync(htmlFile);
-            }
-
-            unlinkElectronIndexPath();
-        });
-
-    } else {
-
-        electronBuilder
-            .build({
-                "projectDir": process.cwd(),
-                "targets": electronBuilder.Platform.MAC.createTarget(),
-                "config": config
-            })
-            .then(() =>
-            {
-                if (generatedHTML) {
-                    fs.unlinkSync(htmlFile);
-                }
-
-                unlinkElectronIndexPath();
-            });
-
-    }
 };
 
 /**
@@ -446,7 +496,15 @@ switch (platform) {
         break;
 
     case "web":
-        buildWeb();
+        buildWeb()
+            .then(() =>
+            {
+                console.log("build done.");
+            })
+            .catch((error) => {
+                console.error(error);
+                process.exit(1);
+            });
         break;
 
     case "steam":
@@ -455,11 +513,14 @@ switch (platform) {
 
     default:
         console.log();
-        console.log("`--platform` can be specified for macOS, Windows, iOS, Android, Web, and Steam.");
+        console.log("`--platform` can be specified for macOS, Windows, iOS, Android, and Web");
         console.log("It is not case sensitive.");
         console.log();
         console.log("For example:");
-        console.log("npx @next2d/builder --platform=web --env=prd");
+        console.log("npx @next2d/builder --platform web --env prd");
+        console.log();
+        console.log("For Debug example:");
+        console.log("npx @next2d/builder --debug --platform web --env prd");
         console.log();
         process.exit(1);
         break;
